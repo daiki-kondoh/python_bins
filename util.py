22c6c3b57373805b925ffe2dc2d16d8a84580fee
kondo-daiki <54925066+daiki-kondoh@users.noreply.github.com>
2023-12-06 07:08:32 +0900

    types_valuesの関数を追記

--- util.py
+++ util.py
@@ -11,6 +11,24 @@ def display_null_number(df):
   display(null_df[['null数']][null_df['null数']>0])
 
 
+#各列に格納されている値のTypeの種類と数をdfの形で表示する関数
+def type_values(df):
+  df_types=df.copy()
+  cols=df_types.columns
+  df_type_dict=pd.DataFrame(np.arange(df_types.shape[1]).reshape(df_types.shape[1], 1),index=cols)
+  df_type_dict['type:数']=df_type_dict[0]
+  for i in range(0,len(cols)):
+    types_list=list(range(df_types.shape[0]))
+    for j in range(0,df_types.shape[0]):
+      value_type=str(type(df_types.at[df_types.index[j],cols[i]]))
+      types_list[j]=value_type
+    df_types[cols[i]]=types_list
+    type_dict=df_types[cols[i]].value_counts().to_dict()
+    df_type_dict['type:数'][i]=type_dict
+  df_type_dict=df_type_dict[['type:数']]
+
+  return df_type_dict
+
 
 #任意の列の値を結合して、新しいIdを作成する関数
 def create_id(df,cols):
