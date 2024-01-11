7411ba2d8a24b91758b27eb68d4be06dd32ca938
kondo-daiki <54925066+daiki-kondoh@users.noreply.github.com>
2024-01-12 06:33:42 +0900

    test_df_value_to_dummy,test_value_to_dummy add

--- test/test_util.py
+++ test/test_util.py
@@ -72,3 +72,76 @@ def test_extract_int_float_data():
     sample_df_2=pd.DataFrame(data=input_data_list_2,columns=input_columns_list)
     diff_df=pd.concat([extract_int_float_data(sample_df_2),expect_df]).drop_duplicates(keep=False)
     assert diff_df.shape==(0, 4)
+
+    def test_value_to_dummy():
+    dic={'?':'NaN','NaN':'--','a':1}
+    assert value_to_dummy('?',dic)=='NaN'
+    assert value_to_dummy('NaN',dic)=='--'
+    assert value_to_dummy('a',dic)==1
+    assert value_to_dummy(0,dic)==0
+
+
+def test_df_value_to_dummy():
+    input_columns_list=['test1-0']
+    input_data_list=[[1],
+                     [2],
+                     ['?'],
+                     [4]]
+    input_expect_columns_list=['test1-0']
+    input_expect_data_list=[[1],
+                     [2],
+                     ['NA'],
+                     [4]]
+    sample_df=pd.DataFrame(data=input_data_list,columns=input_columns_list)
+    expect_df=pd.DataFrame(data=input_expect_data_list,columns=input_expect_columns_list)
+    dic={'?':'NA'}
+    diff_df=pd.concat([df_value_to_dummy(sample_df,dic),expect_df]).drop_duplicates(keep=False) 
+    assert diff_df.shape==(0, 1)
+    
+    input_columns_list_2=['test2-0']
+    input_data_list_2=[[1],
+                     [2],
+                     ['?'],
+                     ['?']]
+    input_expect_columns_list_2=['test2-0']
+    input_expect_data_list_2=[[1],
+                     [2],
+                     ['NA'],
+                     ['NA']]
+    sample_df=pd.DataFrame(data=input_data_list_2,columns=input_columns_list_2)
+    expect_df=pd.DataFrame(data=input_expect_data_list_2,columns=input_expect_columns_list_2)
+    dic={'?':'NA'}
+    diff_df=pd.concat([df_value_to_dummy(sample_df,dic),expect_df]).drop_duplicates(keep=False) 
+    assert diff_df.shape==(0, 1)
+    
+    input_columns_list_3=['test3-0']
+    input_data_list_3=[[1],
+                     [2],
+                     ['?'],
+                     ['---']]
+    input_expect_columns_list_3=['test3-0']
+    input_expect_data_list_3=[[1],
+                     [2],
+                     ['NA'],
+                     ['NA']]
+    sample_df=pd.DataFrame(data=input_data_list_3,columns=input_columns_list_3)
+    expect_df=pd.DataFrame(data=input_expect_data_list_3,columns=input_expect_columns_list_3)
+    dic={'?':'NA','---':'NA'}
+    diff_df=pd.concat([df_value_to_dummy(sample_df,dic),expect_df]).drop_duplicates(keep=False) 
+    assert diff_df.shape==(0, 1)
+
+    input_columns_list_4=['test4-0']
+    input_data_list_4=[[1],
+                     [2],
+                     ['?'],
+                     ['---']]
+    input_expect_columns_list_4=['test4-0']
+    input_expect_data_list_4=[[1],
+                     [2],
+                     ['NA'],
+                     ['']]
+    sample_df=pd.DataFrame(data=input_data_list_4,columns=input_columns_list_4)
+    expect_df=pd.DataFrame(data=input_expect_data_list_4,columns=input_expect_columns_list_4)
+    dic={'?':'NA','---':''}
+    diff_df=pd.concat([df_value_to_dummy(sample_df,dic),expect_df]).drop_duplicates(keep=False) 
+    assert diff_df.shape==(0, 1)
