ec4e5d63731d27d16760589827ff117933337129
kondo-daiki <54925066+daiki-kondoh@users.noreply.github.com>
2024-01-14 21:30:59 +0900

    kfold_score_RamdomForestClassifier add

--- prediction.py
+++ prediction.py
@@ -7,7 +7,18 @@ from sklearn.model_selection import KFold
 
 
 def kfold_score_RandomForestClassifier(x,y,n_split):
+    '''
+    引数：
+    x=x_train
+    y=y_train
+    n_split=k-fold法における分割の数
+    ーーーー
+    戻り値：
+    scores_list=accuracyのlist
+    predict_model=分類の予測モデル
+    '''
+    def kfold_score_RandomForestClassifier(x,y,n_split):
     cv = KFold(n_splits=n_split, random_state=1, shuffle=True)
-    model = RandomForestClassifier()
-    scores = cross_val_score(model, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
-    return scores,model
+    predicti_model = RandomForestClassifier()
+    scores_list = cross_val_score(predicti_model, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    return scores_list,predicti_model
