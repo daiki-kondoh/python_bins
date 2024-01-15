ca6e1f8fd018bd16b454e4ab4db213ec4c85fd16
kondo-daiki <54925066+daiki-kondoh@users.noreply.github.com>
2024-01-16 07:32:30 +0900

    test_cul_importance add

--- test/test_prediction.py
+++ test/test_prediction.py
@@ -17,3 +17,13 @@ def test_kfold_score_RandomForestClassifier():
     score_list_2,predict_model=kfold_score_RandomForestClassifier(x,y,4)
     #乱数が固定されているかテスト
     assert all(score_list_1==score_list_2)==True
+
+def test_cul_importance():
+    data=load_wine()
+    x=pd.DataFrame(data.data)
+    y=pd.DataFrame(data.target)
+    score_list_1,model=kfold_score_RandomForestClassifier(x,y,2)
+    assert  len(cul_importance(x,y,model,5))==5
+    assert  len(cul_importance(x,y,model,5).keys())==5
+    assert  max(list(cul_importance(x,y,model,5).values()))==list(cul_importance(x,y,model,5).values())[0]
+    assert  min(list(cul_importance(x,y,model,5).values()))==list(cul_importance(x,y,model,5).values())[4]
